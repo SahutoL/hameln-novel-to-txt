4149779c911f42230c7adc0629f25fac40c491a3
SahutoL <96646448+SahutoL@users.noreply.github.com>
2024-07-23 20:47:44 +0900

    app.py を更新

--- app.py
+++ app.py
@@ -100,4 +100,4 @@ def download_novel(nid):
         return jsonify({"error": "Novel not found or scraping not completed"}), 404
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False)
