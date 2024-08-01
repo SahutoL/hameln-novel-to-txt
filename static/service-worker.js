178d69489edc3c0f3b877c1952390beb8b674f54
SahutoL <96646448+SahutoL@users.noreply.github.com>
2024-08-01 22:02:59 +0900

    updated service-worker.js

--- static/service-worker.js
+++ static/service-worker.js
@@ -1,19 +1,48 @@
+const CACHE_NAME = 'hameln-txt-cache-v2';
+const urlsToCache = [
+  '/',
+  '/static/icons/icon-192x192.png',
+  '/static/icons/icon-512x512.png',
+];
+
 self.addEventListener('install', function(event) {
   event.waitUntil(
-    caches.open('hameln-txt-cache').then(function(cache) {
-      return cache.addAll([
-        '/',
-        '/static/icons/icon-192x192.png',
-        '/static/icons/icon-512x512.png',
-      ]);
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(urlsToCache);
     })
   );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function(event) {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
 });
 
 self.addEventListener('fetch', function(event) {
   event.respondWith(
     caches.match(event.request).then(function(response) {
-      return response || fetch(event.request);
+      return response || fetch(event.request).then(function(response) {
+        if (!response || response.status !== 200 || response.type !== 'basic') {
+          return response;
+        }
+        const responseToCache = response.clone();
+        caches.open(CACHE_NAME).then(function(cache) {
+          cache.put(event.request, responseToCache);
+        });
+        return response;
+      });
     })
   );
 });
